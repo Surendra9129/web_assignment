f748607d76ed3ac7f3e05e8a6220066339ccfc19
Surendra9129 <91534930+Surendra9129@users.noreply.github.com>
2021-12-22 21:45:19 +0530

    done some styling

---
+++ src/file.js
@@ -0,0 +1,6 @@
+const im="https://v4.webpack.js.org/bf176a25b4f8227fea804854c98dc5e2.png"
+
+function image() {
+    return im;
+}
+exports {image}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import('./index.css')
+import { image } from './file';
+const h1=document.createElement('h1');
+h1.innerText="hello",
+h1.classList.add('redColor');
+document.getElementById('root').appendChild(h1);
+
+const pic=document.createElement('img');
+pic.src="image";
+document.getElementById('root').appendChild(pic)
